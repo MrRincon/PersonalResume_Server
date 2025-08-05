74adf9e4f9b1370cafcc21ce62ac07a9038a80ec
Alam Rincon <alamrdrgz98@gmail.com>
2025-08-05 13:40:04 +0100

    Encryption patch - the 'hex' problem

--- serverRequests.js
+++ serverRequests.js
@@ -59,7 +59,7 @@ async function sendNotificationEmail(toEmail, fromName, fromEmail, messageConten
 // Function for data encryption
 function encrypt(text) {
   const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY, "hex"), iv);
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return iv.toString("hex") + ":" + encrypted;
@@ -70,7 +70,7 @@ function decrypt(text) {
   const [ivHex, encryptedText] = text.split(":");
   const iv = Buffer.from(ivHex, "hex");
   const encrypted = Buffer.from(encryptedText, "hex");
-  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY, "hex"), iv);
   let decrypted = decipher.update(encrypted, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
